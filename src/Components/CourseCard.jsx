e4d075c4a35b0344dcf02b7dd08f8f6108f75942
MANVEERSINGH <singhmanveer645@gmail.com>
2024-09-15 14:56:24 +0530

    updated course card configuruation

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -25,7 +25,9 @@ function CourseCard({ data }) {
   const navigate = useNavigate();
   console.log("courses", courses);
   async function handleDeleteCourse() {
-    if (window.confirm(`Are you sure you want to delete ${data.title} course?`)) {
+    if (
+      window.confirm(`Are you sure you want to delete ${data.title} course?`)
+    ) {
       console.log(data);
       await dispatch(deleteCourse(data._id));
       await dispatch(getAllCourses());
@@ -85,52 +87,46 @@ function CourseCard({ data }) {
   //   };
 
   return (
-    <div className="relative">
+    // <div className="relative">
+    <div className="text-white p-2 sm:w-[22rem] rounded-lg cursor-pointer  overflow-hidden bg-zinc-700">
       <div
+        className="overflow-hidden "
         onClick={() => navigate("/course/description", { state: { ...data } })}
-        className="text-white w-[250px] p-4 sm:w-[20rem] md:w-[22rem] h-[440px]  rounded-lg cursor-pointer  overflow-hidden bg-zinc-700 relative "
       >
-        <div className="overflow-hidden">
-          <img
-            className="rounded-tl-lg rounded-tr-lg group-hover:scale=[1,2] transition-all ease-in-out diration-300"
-            src={data?.thumbnail?.secure_url}
-            alt="course thumbnail"
-          />
-          <div className="p-3 space-y-1 text-white">
-            <h2 className="text-xl font-bold text-yellow-500 line-clamp-2">
-              {data?.title}
-            </h2>
-            <p className="line-clamp-2">{data?.description}</p>
-            <p className="font-semibold">
-              <span className="text-yellow-500 font-bold">Category : </span>
-              {data?.category}
-            </p>
-            <p className="font-semibold">
-              <span className="text-yellow-500 font-bold">
-                Total lectures :{" "}
-              </span>
-              {data?.numberOfLectures}
-            </p>
-            <p className="font-semibold">
-              <span className="text-yellow-500 font-bold">Instructor : </span>
-              {data?.createdBy}
-            </p>
-          </div>
+        <img
+          className="rounded-tl-lg h-[12rem] w-full rounded-tr-lg group-hover:scale=[1,2] transition-all ease-in-out diration-300"
+          src={data?.thumbnail?.secure_url}
+          alt="course thumbnail"
+        />
+        <div className="p-3 space-y-1 text-white">
+          <h2 className="text-xl font-bold text-yellow-500 line-clamp-2">
+            {data?.title}
+          </h2>
+          <p className=" truncate">{data?.description}</p>
+          <p className="font-semibold">
+            <span className="text-yellow-500 font-bold">Category : </span>
+            {data?.category}
+          </p>
+          <p className="font-semibold">
+            <span className="text-yellow-500 font-bold">Total lectures : </span>
+            {data?.numberOfLectures}
+          </p>
+          <p className="font-semibold">
+            <span className="text-yellow-500 font-bold">Instructor : </span>
+            {data?.createdBy}
+          </p>
         </div>
       </div>
-
       {userData?.role === "ADMIN" && (
-        <p className="mt-2 text-center space-x-2">
-          <button
-            className="p-2 bg-red-600 absolute m-auto bottom-4 left-6 font-serif hover:bg-red-500 rounded-lg"
-            onClick={handleDeleteCourse}
-          >
-            Delete Course
-          </button>
-        </p>
+        <button
+          className="p-2 ml-3 bg-red-600 m-auto px-4 font-serif hover:bg-red-500 rounded-lg"
+          onClick={handleDeleteCourse}
+        >
+          Delete Course
+        </button>
       )}
-      
     </div>
+    // </div>
   );
 }
 
